dad7d8478a76120970afc669624bebd71ff44081
AttilaTurkoz <atiturkoz@gmail.com>
2025-07-13 03:55:04 -0600

    package json fx

--- src/store/useCounter.jsx
+++ src/store/useCounter.jsx
@@ -1,9 +1,18 @@
 import { create } from "zustand";
+import axios from "axios";
 
-export const useCounter = create((set) => {
+export const useCounter = create((set, get) => {
   return {
-    counter: 10,
-    incrCounter: () => set((state) => ({ counter: state.counter + 1 })),
-    decrCounter: () => set((state) => ({ counter: state.counter - 1 })),
+    counter: 1,
+    incrCounter: async () => {
+      const { data } = await axios.get(
+        "https://randomuser.me/api/?results=" + (get().counter + 1)
+      );
+      console.log(data);
+      console.log(data.results.length + 1);
+      set({
+        counter: data.results.length,
+      });
+    },
   };
 });
